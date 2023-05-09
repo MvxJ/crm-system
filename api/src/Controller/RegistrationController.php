603717fa49313370ef2e6c152d83eef5f7793557
mvxj <mvxj11@gmail.com>
2023-05-09 21:59:04 +0200

    Initialize CRM App

---
+++ api/src/Controller/RegistrationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+#[Route('/api', name: 'api_')]
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'registration')]
+    public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $em = $doctrine->getManager();
+        $decoded = json_decode($request->getContent());
+        $email = $decoded->email;
+        $plaintextPassword = $decoded->password;
+  
+        $user = new User();
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $user->setEmail($email);
+        $user->setUsername($email);
+        $em->persist($user);
+        $em->flush();
+  
+        return $this->json(['message' => 'Registered Successfully']);
+    }
+}
